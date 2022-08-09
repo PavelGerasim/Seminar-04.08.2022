58803d3a106690fb1d1b106c5df7ea1bc0af4ced
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-09 16:31:40 +0400

    Создал программу для определения чётности числа

---
+++ Выяснить чётное ли заданное число/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число для проверки: ");
+int x = int.Parse(Console.ReadLine());
+if(x%2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число НЕ является чётным");
+}
