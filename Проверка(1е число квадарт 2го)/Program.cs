d44dffc8f1b54d79b00a9d5270fdb4ee31c9f293
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-09 16:01:05 +0400

    start2

---
+++ Проверка(1е число квадарт 2го)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число для проверки: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число для проверки: ");
+int y = int.Parse(Console.ReadLine());
+if(x*x == y) {
+    Console.WriteLine("Yes");
+}
+else {
+    Console.WriteLine("No");
+}
