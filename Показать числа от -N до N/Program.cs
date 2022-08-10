f632e8711f9da729b5684947d1b9542dd3623b15
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-10 10:13:33 +0400

    программа числа от -N до N

---
+++ Показать числа от -N до N/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Укажите первое число диапазона");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Укажите второе число диапазона");
+int y = int.Parse(Console.ReadLine());
+int count = x;
+Console.WriteLine(" ");
+
+while(count <= y)
+{
+    if(count <= y)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+    if(count > y)
+    {
+        Console.WriteLine("Представлены все числа диапазона");
+    }
+}
+Console.WriteLine(" ");
