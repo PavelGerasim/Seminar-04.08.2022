b9dd5a792afdfe3e3159e90fdf6c8d3bdd7758b7
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-11 17:20:20 +0400

    Кратность числа

---
+++ Кратно ли число заданному/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число для проверки кратности: ");
+int y = int.Parse(Console.ReadLine());
+
+if(y % x == 2)
+{
+    Console.WriteLine("Да, второе число кратно первому");
+}
+else
+{
+    Console.WriteLine("Нет, кратности не наблюдается");
+}
