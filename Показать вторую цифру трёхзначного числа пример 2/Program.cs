84b0a8d2e6f68dad86126e8b0fe2c67473b3ed5d
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-11 23:36:00 +0400

    Исправлено

--- Показать вторую цифру трёхзначного числа пример 2/Program.cs
+++ Показать вторую цифру трёхзначного числа пример 2/Program.cs
@@ -1,14 +1,21 @@
-﻿Console.WriteLine("Укажите трёхзначное число: ");
+﻿Console.WriteLine("Случайно нашёл способ не использовать сложные формулы, но первый вариант оставлю (я так долго его делал)");
+Console.WriteLine("Введите трёхзначное число: ");
 int x = int.Parse(Console.ReadLine());
-int y = x;
+int y = x % 10;
 
-if(x > 99)
+if(x > 99 && x < 1000)
 {
-    y = x % 11;
-
-    Console.WriteLine(y);
+    y = x / 10;
+    int z = y;
+    
+    if(y > 9 && y < 100)
+    {
+        z = y % 10;
+    }
+Console.WriteLine(z);
 }
+
 else
 {
-    Console.WriteLine("Это число меньше трёхзначного");
+    Console.WriteLine("Это число не трёхзначное");
 }
