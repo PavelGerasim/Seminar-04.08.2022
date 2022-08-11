c4de5cf1b3b6ce5e771bc05675371b97990505b8
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-11 17:11:32 +0400

    Программа удаление 2й цифры

---
+++ Удаление второй цифры трёхзначного числа/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int y = 3;
+int z = 1;
+
+while(y > 1)
+{
+    z *= 10;
+    y--;
+}
+int x1 = (x % z) + (x / (z * 10)) * z;
+Console.WriteLine(x1);
