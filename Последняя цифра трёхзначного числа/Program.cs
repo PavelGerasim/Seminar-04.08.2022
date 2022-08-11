c1e5edaec8d579c955840194c968aef3e958957f
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-11 10:40:59 +0400

    Создал программу 3я цифра числа

---
+++ Последняя цифра трёхзначного числа/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Укажите трёхзначное число: ");
+int x = int.Parse(Console.ReadLine());
+
+if(x > 99 && x < 1000)
+{
+    Console.WriteLine(x % 10);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
