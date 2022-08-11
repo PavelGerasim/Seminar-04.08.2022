dde76e18214db0f200cb6210850eaf0570d49054
Pavel <Pavel_Gerasimov_98@mail.ru>
2022-08-11 22:54:42 +0400

    Случайно нашёл способ решить без сложной формулы

---
+++ Показать вторую цифру трёхзначного числа пример 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Укажите трёхзначное число: ");
+int x = int.Parse(Console.ReadLine());
+int y = x;
+
+if(x > 99)
+{
+    y = x % 11;
+
+    Console.WriteLine(y);
+}
+else
+{
+    Console.WriteLine("Это число меньше трёхзначного");
+}
